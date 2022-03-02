6980fe1fff55361b0ed5538da12496ed439dcd1d
francescacella <francesca.cella@spindox.it>
2022-03-02 13:07:22 +0100

    Add comments

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const webPush = require('web-push');
-// Important: Generate public and private keys for the PWA subscription before running this statement
+// Important: Generate and store public and private keys for the PWA subscription before running this statement
+// For generating keys: webPush.generateVAPIDKeys()
 webPush.setVapidDetails(
   'https://serviceworke.rs/',
   process.env.VAPID_PUBLIC_KEY,
@@ -19,7 +20,12 @@ app.use(express.json());
 
 const { getShift, getPublicKey, postSubscription } = require('./routes');
 app.get('/', getShift);
+// Public key needed for encription:
+// "From the server-side, the whole process has to be encrypted with public and private keys for security reasons
+// - allowing everyone to send push messages unsecured using your app would be a terrible idea."
+// https://jrconlin.github.io/WebPushDataTestPage/
 app.get('/public-key', getPublicKey);
+// Subscription details sent from the client
 app.post('/subscription', postSubscription);
 
 app.listen(process.env.PORT);
